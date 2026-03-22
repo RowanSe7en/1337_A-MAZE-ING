446a65cf3bd317c0e9b01d20e6c2ead3f4e94fe2
RowanSe7en <bilal.rouane702@gmail.com>
2026-03-22 11:34:05 +0100

    emoji rendering is bullshit

--- algorithm/start.py
+++ algorithm/start.py
@@ -1,7 +1,7 @@
 import random
 
 entry = (0, 0)
-exit_ = (2, 6)
+exit_ = (6, 7)
 
 parents = {}
 path_list = []
@@ -298,13 +298,13 @@ class MazeGenerator:
         return path_str
 
 
-width = 7
-height = 3
+width = 9
+height = 7
 # seed = 22
 seed = 10
 
 p = MazeGenerator(width, height, seed)
-# p.where_is_42()
+p.where_is_42()
 maze = p.generate_perfect_maze()
 # maze = p.generate_non_perfect_maze()
 
@@ -368,6 +368,85 @@ def ascii_render():
 
 ascii_render()
 
+# def emoji_render():
+#     path_coords = set()
+#     cur = exit_
+
+#     while cur in parents:
+#         py, px, _ = parents[cur]
+#         path_coords.add(cur)
+#         cur = (py, px)
+
+#     path_coords.add(entry)
+
+#     for i in range(1, 10):
+#         print(f"{i}\uFE0F\u20E3", end=" ")
+#     for i in range(0, 6):
+#         print(f"{i}\uFE0F\u20E3", end=" ")
+#     print()
+
+#     for y in range(height):
+
+#         print("⬛", end="")
+        
+#         for x in range(width):
+#             if maze[y][x] & (1 << 0):
+#                 print("⬛⬛", end="")
+#             else:
+#                 xx = 1 if x < width - 1 else 0
+
+#                 if (y, x) in path_coords and ((y > 0 and (y - 1, x) in path_coords) or (y - 1, x) == entry):
+#                     left = "🟩"
+#                     content = "⬛"
+
+#                 elif y > 0 and maze[y][x] != 15 and maze[y - 1][x + xx] == 16:
+#                     left = "⬜"
+#                     content = "🟦"
+
+#                 else:
+#                     left = "⬜"
+#                     content = "⬛"
+
+#                 print(left + content, end="")
+#         print("")
+
+#         for x in range(width):
+#             # if x == 0:
+#             #     print("⬛", end="")
+#             if (y, x) in path_coords and ((y, x - 1) in path_coords or (y, x - 1) == entry) and not maze[y][x] & 1 << 3:
+#                 left = "🟩"
+#             elif (maze[y][x] == 15 or maze[y][x] & 1 << 3):
+#                 left = "⬛"
+#             elif (maze[y][x] == 16):
+#                 left = "🟦"
+#             else:
+#                 left = "⬜"
+
+#             if (y, x) == entry:
+#                 content = "🟦"
+#             elif (y, x) == exit_:
+#                 content = "🟪"
+#             elif (y, x) in path_coords:
+#                 content = "🟩"
+#             elif maze[y][x] == 16 and maze[y][x + 1] == 16:
+#                 content = "🟦"
+#             elif maze[y][x] == 16 and maze[y][x + 1] & 1 << 1:
+#                 content = "🟦"
+#             else:
+#                 content = "⬜"
+
+#             print(left + content, end="")
+
+#         right = "⬛" if maze[y][width - 1] & (1 << 1) else ""
+#         print(right)
+
+#     for x in range(width):
+#         if maze[height - 1][x] & (1 << 2):
+#             print("⬛⬛", end="")
+#         else:
+#             print("⬜⬜", end="")
+#     print("⬛")
+
 def emoji_render():
     path_coords = set()
     cur = exit_
@@ -387,50 +466,63 @@ def emoji_render():
 
     for y in range(height):
 
-        print("🧱", end="")
+        print("⬛", end="")
         
         for x in range(width):
             if maze[y][x] & (1 << 0):
-                print("🧱🧱", end="")
+                print("⬛⬛", end="")
             else:
-                if (y, x) in path_coords and ((y - 1, x) in path_coords or (y - 1, x) == entry):
-                    print("🔳🧱", end="")
+                xx = 1 if x < width - 1 else 0
+
+                if (y, x) in path_coords and ((y > 0 and (y - 1, x) in path_coords) or (y - 1, x) == entry):
+                    left = "🟩"
+                    content = "⬛"
+
+                elif y > 0 and maze[y][x] != 15 and maze[y - 1][x + xx] == 16:
+                    left = "⬜"
+                    content = "🟦"
+
                 else:
-                    print("⬜🧱", end="")
+                    left = "⬜"
+                    content = "⬛"
+
+                print(left + content, end="")
         print("")
 
         for x in range(width):
             # if x == 0:
-            #     print("🧱", end="")
+            #     print("⬛", end="")
             if (y, x) in path_coords and ((y, x - 1) in path_coords or (y, x - 1) == entry) and not maze[y][x] & 1 << 3:
-                left = "🔳"
-            elif (maze[y][x] == 15 or maze[y][x] & 1 << 3):
-                left = "🧱"
-            elif (maze[y][x] == 16):
-                left = "🟥"
+                left = "🟩"
+            elif maze[y][x] & 1 << 3 or maze[y][x] & 1 << 4:
+                left = "⬛"
             else:
                 left = "⬜"
 
             if (y, x) == entry:
-                content = "🏂"
+                content = "🟦"
             elif (y, x) == exit_:
-                content = "🚗"
+                content = "🟪"
             elif (y, x) in path_coords:
-                content = "🔳"
+                content = "🟩"
+            elif maze[y][x] == 16:
+                content = "🟦"
+            # elif maze[y][x] == 16 and maze[y][x + 1] & 1 << 1:
+            #     content = "🟦"
             else:
                 content = "⬜"
 
             print(left + content, end="")
 
-        right = "🧱" if maze[y][width - 1] & (1 << 1) else ""
+        right = "⬛" if maze[y][width - 1] & (1 << 1) else ""
         print(right)
 
     for x in range(width):
         if maze[height - 1][x] & (1 << 2):
-            print("🧱🧱", end="")
+            print("⬛⬛", end="")
         else:
             print("⬜⬜", end="")
-    print("🧱")
+    print("⬛")
 
 
 emoji_render()
